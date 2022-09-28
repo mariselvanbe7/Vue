2d53bfecb76ff6e651a3da14bb6ffbe1506fa884
mariselvanbe7 <1810061@nec.edu.in>
2022-09-28 10:02:30 +0530

    small app

---
+++ src/App.vue
@@ -0,0 +1,116 @@
+<template>
+<section>
+ <header><h1>My Friends</h1></header> 
+  <ul>
+   <friend-contact
+    name = "Thangamadu"
+    phone-number = "6574839201"
+    email-address="thanga@local.com"></friend-contact>
+  
+   <friend-contact 
+   name = "Thiru"
+    phone-number = "9087654321"
+    email-address="thiru@local.com"></friend-contact>
+  </ul>
+</section>
+
+</template>
+
+<script>
+import FriendContact from './components/FriendContact.vue';
+ //import FriendContact from './components/FriendContact.vue';
+ export default {
+  components: { FriendContact },
+  data() {
+    return {
+      friends:[
+        {
+          Id: 'loose1',
+          name: 'dfhdfhfhdfudsuif',
+          phone: '7653475348',
+          email: 'loose1@gamill.com'
+
+        },
+
+        {
+          Id: 'loose2',
+          name: 'gdfhghguguidhg',
+          phone: '8924623784',
+          email: 'loose2@gamill.com'
+
+        }
+
+      ]
+      
+    }
+  }
+ }
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+}
+
+html {
+  font-family: 'Jost', sans-serif;
+}
+
+body {
+  margin: 0;
+}
+
+header {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 3rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  background-color: hsl(52, 81%, 50%);
+  color: white;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app ul {
+  margin: 0;
+  padding: 0;
+  list-style: none;
+}
+
+#app li {
+  box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
+  margin: 1rem auto;
+  border-radius: 10px;
+  padding: 1rem;
+  text-align: center;
+  width: 90%;
+  max-width: 40rem;
+}
+
+#app h2 {
+  font-size: 2rem;
+  border-bottom: 4px solid #ccc;
+  color: #d91317;
+  margin: 0 0 1rem 0;
+  font-family: Lucida Console;
+}
+
+#app button {
+  font: inherit;
+  cursor: pointer;
+  border: 1px solid hwb(192 11% 10%);
+  background-color: #0dbdd8;
+  color: white;
+  padding: 0.05rem 1rem;
+  box-shadow: 1px 1px 2px rgba(0, 0, 0, 0.26);
+}
+
+#app button:hover,
+#app button:active {
+  background-color: #ec3169;
+  border-color: #ec3169;
+  box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+
+</style>
---
+++ src/components/FriendContact.vue
@@ -0,0 +1,41 @@
+<template>
+    <li>
+        <h2>{{name}}</h2>
+        <button @click="toggleDetails">Show details</button>
+        <ul v-if="detailsAreVisible">
+            <li><strong>Phone :</strong>{{phoneNumber}}</li>
+            <li><strong>Email :</strong>{{emailAddress}}</li>
+        </ul>
+    </li>
+    
+</template>
+
+<script>
+export default{
+   props :[
+    'name',
+    'phoneNumber',
+    'emailAddress'
+   ],
+    data(){
+        return {
+            detailsAreVisible : false,
+            friends :{
+                
+          Id: 'loose1',
+          name: 'dfhdfhfhdfudsuif',
+          phone: '7653475348',
+          email: 'loose1@gamill.com'
+
+        
+            },
+        };
+    },
+    methods: {
+        toggleDetails() {
+            this.detailsAreVisible = !this.detailsAreVisible;
+        }
+    }
+}
+
+</script>
